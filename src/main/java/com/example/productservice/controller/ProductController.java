da190a9db1dc21810419fd14024755085e6f3aaa
nishilpjpt <ngp.talwart@gmail.com>
2023-09-09 00:10:58 +0530

    Product service

---
+++ src/main/java/com/example/productservice/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.example.productservice.controller;
+
+import com.example.productservice.dto.ProductRequest;
+import com.example.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+}
